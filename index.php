5e1b6684b56645d9bed8ade28ae686f2045d6345
Nelson Moller <nmoller.c@gmail.com>
2017-07-15 10:34:04 -0400

    Add scaffolding to start working in the app without losing a view to SB-admin dashboard to get inspiration.

--- index.php
+++ index.php
@@ -20,52 +20,70 @@ $container['view'] = function ($c) {
     $view = new \Slim\Views\Mustache([
         'cache' => __DIR__.'/cache/mustache',
         'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/src/views'),
-        'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/src/views/partials'),
-        "displayErrorDetails" => true,
+        'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/src/views/partials')
     ]);
     return $view;
 };
 
-$app->get('/', function($request, $response, $args){
-    return $this->view->render($response, 'index.mustache',
-        ['page_title' => 'Home']
-    );
+$app->group('/SB-Admin', function() use ($app) {
+    $app->get('', function($request, $response, $args){
+        return $this->view->render($response, 'index.mustache',
+            ['page_title' => 'Home', 'page' => 'index']
+        );
+    });
+
+    $app->get('/index', function($request, $response, $args){
+        return $this->view->render($response, 'index.mustache',
+            ['page_title' => 'Home']
+        );
+    });
+
+    $app->get('/charts', function($request, $response, $args){
+        return $this->view->render($response, 'charts.mustache',
+            ['page_title' => 'Charts']
+        );
+    })->setName('charts');
+
+    $app->get('/tables', function($request, $response, $args){
+        return $this->view->render($response, 'tables.mustache',
+            ['page_title' => 'Tables']
+        );
+    })->setName('tables');
+
+    $app->get('/forms', function($request, $response, $args){
+        return $this->view->render($response, 'forms.mustache',
+            ['page_title' => 'Forms']
+        );
+    })->setName('forms');
+
+    $app->get('/bootstrap-grid', function($request, $response, $args){
+        return $this->view->render($response, 'bootstrap-grid.mustache',
+            ['page_title' => 'Bootstrap-grid']
+        );
+    })->setName('bootstrap-grid');
+
+    $app->get('/bootstrap-elements', function($request, $response, $args){
+        return $this->view->render($response, 'bootstrap-elements.mustache',
+            ['page_title' => 'Bootstrap-elements']
+        );
+    })->setName('bootstrap-elements');
 });
 
-$app->get('/index', function($request, $response, $args){
-    return $this->view->render($response, 'index.mustache',
-        ['page_title' => 'Home']
-    );
+$app->group('/app', function () use ($app) {
+    $view = new \Slim\Views\Mustache([
+        'cache' => __DIR__.'/cache/mustache',
+        'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/src/views/app'),
+        'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/src/views/app/partials'),
+    ]);
+
+    $app->get('', function($request, $response, $args) use ($view){
+        $this->view = $view;
+        return $this->view->render($response, 'index.mustache',
+            ['page_title' => 'Home', 'page' => 'index']
+        );
+    });
 });
 
-$app->get('/charts', function($request, $response, $args){
-    return $this->view->render($response, 'charts.mustache',
-        ['page_title' => 'Charts']
-    );
-})->setName('charts');
 
-$app->get('/tables', function($request, $response, $args){
-    return $this->view->render($response, 'tables.mustache',
-        ['page_title' => 'Tables']
-    );
-})->setName('tables');
-
-$app->get('/forms', function($request, $response, $args){
-    return $this->view->render($response, 'forms.mustache',
-        ['page_title' => 'Forms']
-    );
-})->setName('forms');
-
-$app->get('/bootstrap-grid', function($request, $response, $args){
-    return $this->view->render($response, 'bootstrap-grid.mustache',
-        ['page_title' => 'Bootstrap-grid']
-    );
-})->setName('bootstrap-grid');
-
-$app->get('/bootstrap-elements', function($request, $response, $args){
-    return $this->view->render($response, 'bootstrap-elements.mustache',
-        ['page_title' => 'Bootstrap-elements']
-    );
-})->setName('bootstrap-elements');
 
 $app->run();
